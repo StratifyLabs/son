840492427fdc2bcad07d48aa2efb377015e00e44
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-01-21 10:13:17 -0700

    cmake updates

--- arm.cmake
+++ arm.cmake
@@ -1,23 +1,26 @@
 
 
 #Add sources to the project
-set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src CACHE INTERNAL "src prefix")
+set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
 list(APPEND SOS_LIB_SOURCELIST ${SOURCES})
 
-set(SOS_LIB_ARCH armv7-m CACHE INTERNAL "sos build ")
-set(SOS_LIB_CONFIG kernel CACHE INTERNAL "sos build config release")
+set(SOS_LIB_TYPE release)
+
+# Kernel libs for armv7-m and armv7e-m
+set(SOS_LIB_ARCH armv7-m)
+set(SOS_LIB_OPTION kernel)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
 
-set(SOS_LIB_ARCH armv7e-m CACHE INTERNAL "sos build ")
-set(SOS_LIB_CONFIG kernel CACHE INTERNAL "sos build config release")
+set(SOS_LIB_ARCH armv7e-m)
+set(SOS_LIB_OPTION kernel)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
 
-#for linking to applications
-set(SOS_LIB_BUILD_FLAGS -mlong-calls CACHE INTERNAL "sos lib build flags")
-set(SOS_LIB_ARCH armv7-m CACHE INTERNAL "sos build ")
-set(SOS_LIB_CONFIG release CACHE INTERNAL "sos build config release")
+#for linking to applications for armv7-m and armv7e-m
+set(SOS_LIB_BUILD_FLAGS -mlong-calls)
+set(SOS_LIB_ARCH armv7-m)
+set(SOS_LIB_OPTION "")
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
 
-set(SOS_LIB_ARCH armv7e-m CACHE INTERNAL "sos build ")
+set(SOS_LIB_ARCH armv7e-m)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
