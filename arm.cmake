3e1353ff9295d843bcb64f7558c18329cd91f9a7
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-26 12:26:31 -0600

    cmake variables update

--- arm.cmake
+++ arm.cmake
@@ -3,16 +3,16 @@
 #Add sources to the project
 set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
-list(APPEND SOS_LIB_SOURCELIST ${SOURCES})
+list(APPEND SOS_SOURCELIST ${SOURCES})
 
-set(SOS_LIB_TYPE release)
+set(SOS_CONFIG release)
 
 # Kernel libs for standard arch's
-set(SOS_LIB_OPTION kernel)
+set(SOS_OPTION kernel)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
 
 #for linking to applications for standard arch's
-set(SOS_LIB_BUILD_FLAGS -mlong-calls)
-set(SOS_LIB_OPTION "")
+set(SOS_BUILD_FLAGS -mlong-calls)
+set(SOS_OPTION "")
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
 
--- link.cmake
+++ link.cmake
@@ -3,12 +3,12 @@
 #Add sources to the project
 set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
-list(APPEND SOS_LIB_SOURCELIST ${SOURCES})
+list(APPEND SOS_SOURCELIST ${SOURCES})
 
-set(SOS_LIB_TYPE release)
-set(SOS_LIB_OPTION "")
-set(SOS_LIB_ARCH link)
+set(SOS_CONFIG release)
+set(SOS_OPTION "")
+set(SOS_ARCH link)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
-set(SOS_LIB_OPTION link)
-set(SOS_LIB_ARCH link)
+set(SOS_OPTION link)
+set(SOS_ARCH link)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
